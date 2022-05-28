17c1b513c0fa9c387fd6ca0194d6f5899b558b1b
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-05-28 17:10:31 +1200

    init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="nav">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </div>
+    <div id="page">
+        
+    </div>
+
+    <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </nav> -->
+    
+    <RouterView />
+</template>
+
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+
+
+<style>
+@import '@/assets/base.css';
+#app{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    background-color: white;
+}
+
+#nav{
+    min-width: 60px;
+    width: 60px;
+    height: 100vh;
+    background-color: black;
+}
+
+#page{
+    width: 100%;
+    height: 100vh;
+}
+</style>
---
+++ src/components/ButtonItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+	name: 'ButtonItem',
+	props: {
+        
+    },
+	
+}
+</script>
+
+<style scoped>
+
+</style>
